c6e2b470d9965f441743cabcb19221f99ec342e8
josemendes85 <150475617+josemendes85@users.noreply.github.com>
2025-07-15 00:05:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './sw.js',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
